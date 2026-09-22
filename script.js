d326b2485a4ad524df1bddb5f415bac05fde6d0b
Sanjay Kushwaha <sanjaykushwaha80676@gmail.com>
2026-09-22 11:25:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let timeDisplay = document.getElementById("timer");
+
+let currentTime = new Date() ;
+
+setInterval(
+	function () {
+		currentTime= new Date
+		timeDisplay.innerHTMl = currentTime.toLocaleString();
+	}, 1000 ;
+) ;
